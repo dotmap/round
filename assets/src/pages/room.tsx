1f96d618fcbcad502e4c2be612d832546b451296
Graham Barber <grahammbarber@gmail.com>
2019-02-19 16:13:07 -0800

    feat(room): add navbar

--- assets/src/pages/room.tsx
+++ assets/src/pages/room.tsx
@@ -3,6 +3,7 @@ import { FC, FormEvent, Fragment, useState, lazy, Suspense } from 'react'
 import { Flex, Button, Card, Text } from '@rebass/emotion'
 
 import Loading from '../components/loading'
+import NavBar from '../components/navbar'
 
 const Estimation = lazy(() =>
   import(/* webpackChunkName: "estimation" */ '../components/estimation')
@@ -19,34 +20,37 @@ const Room: FC<RoomProps> = ({ room }) => {
   const estimateValues = ['0', '1', '2', '3', '5', '8', '13', '20', '40']
 
   return (
-    <Flex
-      justifyContent="center"
-      alignItems="center"
-      css={{ minHeight: '90vh' }}
-    >
-      {!nameSelected ? (
-        <Card p={3} boxShadow={[0]} borderRadius="default" bg="white">
-          <Text>Enter a name:</Text>
-          <Card
-            as="input"
-            type="text"
-            value={username}
-            onChange={(e: FormEvent<HTMLInputElement>) =>
-              setUsername(e.currentTarget.value)
-            }
-          />
-          <Button onClick={() => setNameSelected(true)}>Submit</Button>
-        </Card>
-      ) : (
-        <Suspense fallback={<Loading />}>
-          <Estimation
-            room={room || ''}
-            username={username}
-            estimateValues={estimateValues}
-          />
-        </Suspense>
-      )}
-    </Flex>
+    <Fragment>
+      <NavBar />
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        css={{ minHeight: 'auto' }}
+      >
+        {!nameSelected ? (
+          <Card p={3} boxShadow={[0]} borderRadius="default" bg="white">
+            <Text>Enter a name:</Text>
+            <Card
+              as="input"
+              type="text"
+              value={username}
+              onChange={(e: FormEvent<HTMLInputElement>) =>
+                setUsername(e.currentTarget.value)
+              }
+            />
+            <Button onClick={() => setNameSelected(true)}>Submit</Button>
+          </Card>
+        ) : (
+          <Suspense fallback={<Loading />}>
+            <Estimation
+              room={room || ''}
+              username={username}
+              estimateValues={estimateValues}
+            />
+          </Suspense>
+        )}
+      </Flex>
+    </Fragment>
   )
 }
 
